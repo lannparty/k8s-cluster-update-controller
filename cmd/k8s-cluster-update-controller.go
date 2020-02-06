233ed5f7227576dcb79a5748addf056bba87e9a0
Isa Aguilar <isa@aguilarstory.com>
2020-02-05 16:58:43 -0800

    move controller logic to internals

--- cmd/k8s-cluster-update-controller.go
+++ cmd/k8s-cluster-update-controller.go
@@ -22,175 +22,18 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
-	"time"
 
-	"github.com/lannparty/k8s-cluster-update-controller/pkg/kubecmd"
+	"github.com/lannparty/k8s-cluster-update-controller/internal/controller"
 	"k8s.io/klog"
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/apimachinery/pkg/util/wait"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/workqueue"
 )
-
-type Controller struct {
-	indexer  cache.Indexer
-	queue    workqueue.RateLimitingInterface
-	informer cache.Controller
-	session  kubernetes.Interface
-}
-
-func NewController(queue workqueue.RateLimitingInterface, indexer cache.Indexer, informer cache.Controller) *Controller {
-	return &Controller{
-		informer: informer,
-		indexer:  indexer,
-		queue:    queue,
-	}
-}
-
-func (c *Controller) processNextItem() bool {
-	// Wait until there is a new item in the working queue
-	key, quit := c.queue.Get()
-	if quit {
-		return false
-	}
-	// Tell the queue that we are done with processing this key. This unblocks the key for other workers
-	// This allows safe parallel processing because two Nodes with the same key are never processed in
-	// parallel.
-	defer c.queue.Done(key)
-
-	// Invoke the method containing the business logic
-	err := c.syncToStdout(key.(string))
-	// Handle the error if something went wrong during the execution of the business logic
-	c.handleErr(err, key)
-	return true
-}
-
-func (c *Controller) setSession(clientset *kubernetes.Clientset) {
-	c.session = clientset
-}
-
-// syncToStdout is the business logic of the controller. In this controller it simply prints
-// information about the node to stdout. In case an error happened, it has to simply return the error.
-// The retry logic should not be part of the business logic.
-func (c *Controller) syncToStdout(key string) error {
-	obj, exists, err := c.indexer.GetByKey(key)
-	if err != nil {
-		klog.Errorf("Fetching object with key %s from store failed with %v\n", key, err)
-		return err
-	}
-
-	if !exists {
-		// Below we will warm up our cache with a Node, so that we will see a delete for one node
-		fmt.Printf("Node %s does not exist anymore\n", key)
-	} else {
-		// Note that you also have to check the uid if you have a local controlled resource, which
-		// is dependent on the actual instance, to detect that a Pod was recreated with the same name
-		targetNode := obj.(*v1.Node).GetName()
-		evictionWaitTimeVar := os.Getenv("EVICTIONWAITTIME")
-		if err != nil {
-			return err
-		}
-		evictionWaitTime, err := strconv.Atoi(evictionWaitTimeVar)
-		if err != nil {
-			return err
-		}
-		if err != nil {
-			return err
-		}
-		validateWaitTimeVar := os.Getenv("VALIDATEWAITTIME")
-		if err != nil {
-			return err
-		}
-		validateWaitTime, err := strconv.Atoi(validateWaitTimeVar)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("Cordoning node %s\n", targetNode)
-		// Set node unscheduable.
-		kubecmd.CordonNode(c.session, targetNode)
-		if err != nil {
-			klog.Errorf("Cordon node %s failed with error %v\n", targetNode, err)
-		}
-		for {
-			err := kubecmd.EvictPodsOnCordonedNodes(c.session, targetNode, "v1beta1")
-			time.Sleep(time.Duration(evictionWaitTime) * time.Second)
-			if err != nil {
-				klog.Errorf("Eviction of pods on %s failed with error %v\n", targetNode, err)
-			} else {
-				break
-			}
-		}
-
-		vitalNamespaces := strings.Split(os.Getenv("VITALNAMESPACES"), ",")
-		for kubecmd.ValidateNamespaces(c.session, vitalNamespaces) == false {
-			fmt.Printf("Waiting for pods on vital namespaces %v to stablize.\n", vitalNamespaces)
-			time.Sleep(time.Duration(validateWaitTime) * time.Second)
-		}
-	}
-	return nil
-}
-
-// handleErr checks if an error happened and makes sure we will retry later.
-func (c *Controller) handleErr(err error, key interface{}) {
-	if err == nil {
-		// Forget about the #AddRateLimited history of the key on every successful synchronization.
-		// This ensures that future processing of updates for this key is not delayed because of
-		// an outdated error history.
-		c.queue.Forget(key)
-		return
-	}
-
-	// This controller retries 5 times if something goes wrong. After that, it stops trying.
-	if c.queue.NumRequeues(key) < 5 {
-		klog.Infof("Error syncing node %v: %v", key, err)
-
-		// Re-enqueue the key rate limited. Based on the rate limiter on the
-		// queue and the re-enqueue history, the key will be processed later again.
-		c.queue.AddRateLimited(key)
-		return
-	}
-
-	c.queue.Forget(key)
-	// Report to an external entity that, even after several retries, we could not successfully process this key
-	runtime.HandleError(err)
-	klog.Infof("Dropping node %q out of the queue: %v", key, err)
-}
-
-func (c *Controller) Run(threadiness int, stopCh chan struct{}) {
-	defer runtime.HandleCrash()
-
-	// Let the workers stop when we are done
-	defer c.queue.ShutDown()
-	klog.Info("Starting Node controller")
-
-	go c.informer.Run(stopCh)
-
-	// Wait for all involved caches to be synced, before processing items from the queue is started
-	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
-		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
-		return
-	}
-
-	for i := 0; i < threadiness; i++ {
-		go wait.Until(c.runWorker, time.Second, stopCh)
-	}
-
-	<-stopCh
-	klog.Info("Stopping Node controller")
-}
-
-func (c *Controller) runWorker() {
-	for c.processNextItem() {
-	}
-}
 
 func main() {
 	var kubeconfig string
@@ -250,13 +93,13 @@ func main() {
 		},
 	}, cache.Indexers{})
 
-	controller := NewController(queue, indexer, informer)
-	controller.setSession(clientset)
+	c := controller.NewController(queue, indexer, informer)
+	c.SetSession(clientset)
 
 	// Now let's start the controller
 	stop := make(chan struct{})
 	defer close(stop)
-	go controller.Run(1, stop)
+	go c.Run(1, stop)
 
 	// Wait forever
 	select {}
---
+++ internal/controller/controller.go
@@ -0,0 +1,177 @@
+package controller
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/lannparty/k8s-cluster-update-controller/pkg/kubecmd"
+
+	"fmt"
+	"os"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog"
+
+	"k8s.io/client-go/tools/cache"
+
+	"k8s.io/client-go/util/workqueue"
+)
+
+type Controller struct {
+	indexer  cache.Indexer
+	queue    workqueue.RateLimitingInterface
+	informer cache.Controller
+	session  kubernetes.Interface
+}
+
+func NewController(queue workqueue.RateLimitingInterface, indexer cache.Indexer, informer cache.Controller) *Controller {
+	return &Controller{
+		informer: informer,
+		indexer:  indexer,
+		queue:    queue,
+	}
+}
+
+func (c *Controller) processNextItem() bool {
+	// Wait until there is a new item in the working queue
+	key, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+	// Tell the queue that we are done with processing this key. This unblocks the key for other workers
+	// This allows safe parallel processing because two Nodes with the same key are never processed in
+	// parallel.
+	defer c.queue.Done(key)
+
+	// Invoke the method containing the business logic
+	err := c.syncToStdout(key.(string))
+	// Handle the error if something went wrong during the execution of the business logic
+	c.handleErr(err, key)
+	return true
+}
+
+func (c *Controller) SetSession(clientset *kubernetes.Clientset) {
+	c.session = clientset
+}
+
+// syncToStdout is the business logic of the controller. In this controller it simply prints
+// information about the node to stdout. In case an error happened, it has to simply return the error.
+// The retry logic should not be part of the business logic.
+func (c *Controller) syncToStdout(key string) error {
+
+	obj, exists, err := c.indexer.GetByKey(key)
+	if err != nil {
+		klog.Errorf("Fetching object with key %s from store failed with %v\n", key, err)
+		return err
+	}
+
+	if !exists {
+		// Below we will warm up our cache with a Node, so that we will see a delete for one node
+		fmt.Printf("Node %s does not exist anymore\n", key)
+	} else {
+		err := c.RollingUpdate(obj)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// handleErr checks if an error happened and makes sure we will retry later.
+func (c *Controller) handleErr(err error, key interface{}) {
+	if err == nil {
+		// Forget about the #AddRateLimited history of the key on every successful synchronization.
+		// This ensures that future processing of updates for this key is not delayed because of
+		// an outdated error history.
+		c.queue.Forget(key)
+		return
+	}
+
+	// This controller retries 5 times if something goes wrong. After that, it stops trying.
+	if c.queue.NumRequeues(key) < 5 {
+		klog.Infof("Error syncing node %v: %v", key, err)
+
+		// Re-enqueue the key rate limited. Based on the rate limiter on the
+		// queue and the re-enqueue history, the key will be processed later again.
+		c.queue.AddRateLimited(key)
+		return
+	}
+
+	c.queue.Forget(key)
+	// Report to an external entity that, even after several retries, we could not successfully process this key
+	runtime.HandleError(err)
+	klog.Infof("Dropping node %q out of the queue: %v", key, err)
+}
+
+func (c *Controller) Run(threadiness int, stopCh chan struct{}) {
+	defer runtime.HandleCrash()
+
+	// Let the workers stop when we are done
+	defer c.queue.ShutDown()
+	klog.Info("Starting Node controller")
+
+	go c.informer.Run(stopCh)
+
+	// Wait for all involved caches to be synced, before processing items from the queue is started
+	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
+		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
+		return
+	}
+
+	for i := 0; i < threadiness; i++ {
+		go wait.Until(c.runWorker, time.Second, stopCh)
+	}
+
+	<-stopCh
+	klog.Info("Stopping Node controller")
+}
+
+func (c *Controller) runWorker() {
+	for c.processNextItem() {
+	}
+}
+
+// RollingUpdate executes the cordon, drain, and validate functions and *blocks*
+// until it returns with or without an error. Blocking limits the draining of
+// nodes to one at a time.
+func (c *Controller) RollingUpdate(obj interface{}) error {
+	// Note that you also have to check the uid if you have a local controlled resource, which
+	// is dependent on the actual instance, to detect that a Pod was recreated with the same name
+	targetNode := obj.(*v1.Node).GetName()
+	evictionWaitTimeVar := os.Getenv("EVICTIONWAITTIME")
+	evictionWaitTime, err := strconv.Atoi(evictionWaitTimeVar)
+	if err != nil {
+		return err
+	}
+	validateWaitTimeVar := os.Getenv("VALIDATEWAITTIME")
+	validateWaitTime, err := strconv.Atoi(validateWaitTimeVar)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Cordoning node %s\n", targetNode)
+	// Set node unscheduable.
+	err = kubecmd.CordonNode(c.session, targetNode)
+	if err != nil {
+		klog.Errorf("Cordon node %s failed with error %v\n", targetNode, err)
+	}
+	for {
+		err = kubecmd.EvictPodsOnCordonedNodes(c.session, targetNode, "v1beta1")
+		time.Sleep(time.Duration(evictionWaitTime) * time.Second)
+		if err != nil {
+			klog.Errorf("Eviction of pods on %s failed with error %v\n", targetNode, err)
+		} else {
+			break
+		}
+	}
+
+	vitalNamespaces := strings.Split(os.Getenv("VITALNAMESPACES"), ",")
+	for kubecmd.ValidateNamespaces(c.session, vitalNamespaces) == false {
+		fmt.Printf("Waiting for pods on vital namespaces %v to stablize.\n", vitalNamespaces)
+		time.Sleep(time.Duration(validateWaitTime) * time.Second)
+	}
+	return nil
+}
