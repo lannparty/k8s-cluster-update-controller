fa8b10cb9be7e14eeafd163a4c1dd8e8cc4d33ba
isaaguilar <isa@aguilarstory.com>
2020-02-21 16:03:25 -0800

    check nodes are actually cordoned

--- pkg/kubecmd/kubecmd_test.go
+++ pkg/kubecmd/kubecmd_test.go
@@ -16,10 +16,45 @@ import (
 func TestCordonNode(t *testing.T) {
 	fakeClient := fake.NewSimpleClientset()
 
+	// Add a couple nodes so we can test cordoner
+	n1 := corev1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node",
+		},
+	}
+	n2 := corev1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node2",
+		},
+	}
+	fakeClient.CoreV1().Nodes().Create(&n1)
+	fakeClient.CoreV1().Nodes().Create(&n2)
+
 	err := CordonNode(fakeClient, "node")
 	if err != nil {
 		t.Errorf("error: %v", err)
 	}
+
+	// expect "node" to be cordoned
+	cordoned1, err := fakeClient.CoreV1().Nodes().Get("node", metav1.GetOptions{})
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+
+	// expect "node2" to remain uncordoned
+	cordoned2, err := fakeClient.CoreV1().Nodes().Get("node2", metav1.GetOptions{})
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+
+	if !cordoned1.Spec.Unschedulable {
+		t.Errorf("Node should be cordoned but is not")
+	}
+
+	if cordoned2.Spec.Unschedulable {
+		t.Errorf("Node should not be cordoned but is")
+	}
+
 }
 
 func TestEvictPodsOnCordonedNodes(t *testing.T) {
